298fe96f9a731fc55f82185197969630de1ae1b5
Vika <vika.olevsk@yandex.ru>
2021-05-30 23:05:41 +0300

    replacement

--- hw5/task2.py
+++
@@ -1,5 +0,0 @@
-with open("task2.txt") as f:
-    for i, line in enumerate(f):
-        n = line.split()
-        print('Line %d contains %d words' % (i+1,len(n)))
-    print('File has %d lines' % (i+1))
--- hw5/task4.py
+++
@@ -1,6 +0,0 @@
-rus_nums = {1: 'Один', 2: 'Два', 3: 'Три', 4: 'Четыре'}
-
-with open('task4.txt', 'r') as f_en, open('task4_ru.txt', 'w') as f_ru:
-    for line in f_en:
-        print(rus_nums.get(int(line[-2])) + line[-5:])
-        f_ru.write(rus_nums.get(int(line[-2])) + line[-5:])
